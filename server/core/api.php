4e79a701eb205082b43f1bf0b7eb4a531b146c39
Alexander Deider <for_inbox@mail.ru>
2014-03-25 03:03:58 +0700

    write Service manager

---
+++ server/core/api.php
@@ -0,0 +1,76 @@
+<?php
+namespace API;
+
+use API\Core\Router;
+use API\Core\Response;
+use API\Core\RouterException;
+use API\Core\ValidatorException;
+
+use API\SDK\IService;
+
+class API
+{
+  private $start;
+  private $router = null;
+  private $response = null;
+
+  public function __construct()
+  {
+    $this->start = microtime(1);
+    $this->router = Router::getInstance();
+    $this->response = new Response();
+  }
+
+  /**
+   * Register service routing to API
+   *
+   * @param SDK\IService $svc
+   * @return $this
+   */
+  public function addService(IService $svc)
+  {
+    $svc->registerRoutes($this->router);
+    return $this;
+  }
+
+  /**
+   * Process API request
+   *
+   */
+  public function run()
+  {
+    //todo: add error codes later
+    $res = array();
+    try {
+      if ($r = $this->router->execURI())
+        $res['result'] = $r;
+    } catch (\Exception $e) {
+      $res['error'] = $e->getMessage();//'Something wrong happened';
+      switch(get_class($e)) {
+        default: $res['type'] = 'internal'; break;
+        case 'API\Core\RouterException': $res['type'] = 'routing'; break;
+        case 'API\Core\ValidatorException': $res['type'] = 'validation'; break;
+      }
+      $res['status'] = 'fail';
+      $res['code'] = 500;
+      $error = $e;
+    }
+
+    $res['time'] = microtime(1) - $this->start;
+
+    $db = Core\DB\DB::getInstance();
+    $res['db'] = array(
+      'count' => $db->getQCount(),
+      'time' =>  $db->getQTime()
+    );
+    $cache = Core\DB\Cache::getInstance();
+    $res['cache'] = array(
+      'count' => $cache->getQCount(),
+      'time' =>  $cache->getQTime()
+    );
+    if(!isset($e)) {
+      $res['status'] = 'ok';
+    }
+    $this->response->send($res);
+  }
+}
